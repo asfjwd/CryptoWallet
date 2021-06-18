7eecc1cb13528a81ff0bbec466db051c22870483
Asif Jawad <aasfjwd@gmail.com>
2021-06-18 20:07:21 +0600

    added watchlist layout, fragment and adapter.

---
+++ app/src/main/java/com/cse2216/cryptowallet/adapters/WatchListAdapter.java
@@ -0,0 +1,87 @@
+package com.cse2216.cryptowallet.adapters;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cse2216.cryptowallet.R;
+import com.cse2216.cryptowallet.classes.domain.Coin;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WatchListAdapter extends RecyclerView.Adapter<WatchListAdapter.WatchListViewHolder> {
+
+    String upArrow = Character.toString((char)(8593));
+    String downArrow = Character.toString((char)(8595));
+    String verticalLine = Character.toString((char)(9087));
+    Integer negativeRed = Color.parseColor("#FF1744");
+    Integer positiveGreen = Color.parseColor("#00E676");
+
+    List<Coin> allCoins;
+    ArrayList<Integer> watchList;
+
+    public WatchListAdapter(List<Coin> allCoins, ArrayList<Integer> watchList){
+        this.allCoins = allCoins;
+        this.watchList = watchList;
+    }
+
+    public WatchListViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.watchlist_item_layout, parent, false);
+        return new WatchListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull WatchListAdapter.WatchListViewHolder holder, int pos) {
+        int position = watchList.get(pos);
+        holder.currencyName.setText(allCoins.get(position).getName());
+        holder.ltp.setText(String.format("%.2f", allCoins.get(position).getLatestPrice()));
+        Double change_24hr = allCoins.get(position).getChange();
+        holder.change_24hr.setText(String.format("%.2f", change_24hr));
+        holder.volume.setText(String.format("%.4f", allCoins.get(position).getVolume()));
+        holder.verticalLine.setText(verticalLine);
+        if(change_24hr > 0){
+            holder.change_24hr.setTextColor(positiveGreen);
+            holder.arrow.setText(upArrow);
+            holder.arrow.setTextColor(positiveGreen);
+            holder.verticalLine.setTextColor(positiveGreen);
+        }
+        else {
+            holder.change_24hr.setTextColor(negativeRed);
+            holder.arrow.setText(downArrow);
+            holder.arrow.setTextColor(negativeRed);
+            holder.verticalLine.setTextColor(negativeRed);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return watchList.size();
+    }
+
+    public class WatchListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgIcon;
+        TextView currencyName, ltp, change_24hr, volume, arrow, verticalLine;
+        public WatchListViewHolder(View itemView) {
+            super(itemView);
+
+            imgIcon = itemView.findViewById(R.id.watchlist_img_icon);
+            currencyName = itemView.findViewById(R.id.watchlist_currency_name);
+            ltp = itemView.findViewById(R.id.watchlist_ltp_id);
+            change_24hr = itemView.findViewById(R.id.watchlist_change_24hr_id);
+            volume = itemView.findViewById(R.id.watchlist_volume_id);
+            arrow = itemView.findViewById(R.id.watchlist_arrow_id);
+            verticalLine = itemView.findViewById(R.id.watchlist_vertical_line);
+
+        }
+    }
+}
--- app/src/main/java/com/cse2216/cryptowallet/fragments/WatchlistFragment.java
+++ app/src/main/java/com/cse2216/cryptowallet/fragments/WatchlistFragment.java
@@ -8,16 +8,62 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.cse2216.cryptowallet.R;
+import com.cse2216.cryptowallet.activities.MainActivity;
+import com.cse2216.cryptowallet.adapters.WatchListAdapter;
 
 import org.jetbrains.annotations.NotNull;
 
-public class WatchlistFragment extends Fragment {
+public class WatchlistFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+
+    private View rootView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView watchListRecyclerView;
+    MainActivity rootActivity;
+
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.watchlist_fragment_layout,container,false);
+        rootView = inflater.inflate(R.layout.watchlist_fragment_layout,container,false);
+        rootActivity = (MainActivity) getActivity();
+        watchListRecyclerView = rootView.findViewById(R.id.watchlist_fragment);
+        swipeRefreshLayout = rootView.findViewById(R.id.watchlist_fragment_swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        watchListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        watchListRecyclerView.setHasFixedSize(true);
+        watchListRecyclerView.setAdapter(new WatchListAdapter(rootActivity.coins, rootActivity.user.watchList));
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+                updateList();
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            updateList();
+        }
+    }
+
+    private void updateList() {
+        swipeRefreshLayout.setRefreshing(true);
+        watchListRecyclerView.setAdapter(new WatchListAdapter(rootActivity.coins, rootActivity.user.watchList));
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        updateList();
     }
 }
